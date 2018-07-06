8fa68b3f163c482ba9707bf510f8f1ed929ed67a
Chris Wisecarver <chris.wisecarver@parsely.com>
2018-07-06 15:15:34 -0400

    Initial Commit

---
+++ AnalyticsSDK/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  AnalyticsSDK
+//
+//  Created by Chris Wisecarver on 7/6/18.
+//  Copyright © 2018 Parse.ly. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ AnalyticsSDK/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  AnalyticsSDK
+//
+//  Created by Chris Wisecarver on 7/6/18.
+//  Copyright © 2018 Parse.ly. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
