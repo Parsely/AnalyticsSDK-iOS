98d3c8979f68fa72097117a7a311b4bac739377a
Chris Wisecarver <chris.wisecarver@parsely.com>
2018-10-19 16:46:36 -0400

    Add playground code

--- MyNewPlayground.playground/Contents.swift
+++ MyNewPlayground.playground/Contents.swift
@@ -1,14 +1,19 @@
-import UIKit
+import Foundation
 
-let doIt: () -> String = {
-    print("woo")
-    return "hoo"
+let dict: [String: Any] = ["idsite": "example.com", "ts": 13123123.13123, "data": ["tags": ["tag1", "tag2", "tag3"], "cust_ts": 13132123123]]
+
+func mkJSON(dic: [String: Any]) -> String {
+    do {
+        let jsonData = try JSONSerialization.data(withJSONObject: dic, options: .prettyPrinted)
+        let jsonText = String(data: jsonData, encoding: .ascii)
+        return jsonText!
+    } catch {
+        print(error.localizedDescription)
+    }
+    return ""
 }
+let jsonString = mkJSON(dic: dict)
 
-func doOtherThings(function: () -> String, otherThing: String) {
-    let yasss: String = function()
-    print(yasss)
-    print(otherThing)
-}
+var wot: Dictionary<String, Any?> = [:]
 
-doOtherThings(function: doIt, otherThing: "HAHAHAHAHA")
+wot.removeValue(forKey: "stinks")
