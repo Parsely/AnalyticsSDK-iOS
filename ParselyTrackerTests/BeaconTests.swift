0f8e28b72d1712db8bb7fa10f70ca0d74818f982
aldraco <a.l.drake713@gmail.com>
2019-01-30 15:41:27 -0700

    Stub tests for Beacon, EngagedTime.

--- ParselyTrackerTests/BeaconTests.swift
+++ ParselyTrackerTests/BeaconTests.swift
@@ -5,5 +5,27 @@
 //  Created by Chris Wisecarver on 11/5/18.
 //  Copyright © 2018 Parse.ly. All rights reserved.
 //
-
+import XCTest
+@testable import ParselyTracker
 import Foundation
+
+class BeaconTests: XCTestCase {
+    let parselyTrackerInstance: Parsely = Parsely.sharedInstance
+    let parselyBeacon: Beacon = Beacon()
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testTrackPageView() {
+        // TODO: test side effects of this function?
+        // parselyBeacon.trackPageView(params: [:], shouldNotSetLastRequest: true)
+        XCTAssert(true)
+    }
+}
+
+
