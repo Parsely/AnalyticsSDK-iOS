587be9a0db58e79c21f2b5b19aa925608379098c
aldraco <a.l.drake713@gmail.com>
2019-02-05 10:49:48 -0700

    Add Sampler tests.

---
+++ ParselyTrackerTests/SamplerTests.swift
@@ -0,0 +1,32 @@
+//
+//  SamplerTests.swift
+//  ParselyTrackerTests
+//
+//  Created by Ashley Drake on 2/5/19.
+//  Copyright © 2019 Parse.ly. All rights reserved.
+//
+import XCTest
+@testable import ParselyTracker
+import Foundation
+
+class SamplerTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testUpdateGlobalHeartbeatInterval() {
+        // tracking a new key should result in changing the global heartbeat interval
+        let sampler = Sampler()
+        let initialHbValue = sampler.heartbeatInterval
+        XCTAssert(initialHbValue == TimeInterval(floatLiteral: 10.5))
+        
+        sampler.trackKey(key: "testKey", duration: TimeInterval(floatLiteral: 10.0))
+        let newHbValue = sampler.heartbeatInterval
+        XCTAssert(newHbValue != initialHbValue, "A shorter duration should decrease the global timeout.")
+        XCTAssert(newHbValue == TimeInterval(floatLiteral: 2.0), "10s duration should account for all completion intervals.")
+    }
+}
