ed1e3adf1a687412dc23ef8ce5d3a0755f4f5b11
aldraco <a.l.drake713@gmail.com>
2019-02-06 12:35:59 -0700

    Add failing test for EngagedTime, tracking separate items.

--- ParselyTrackerTests/EngagedTimeTests.swift
+++ ParselyTrackerTests/EngagedTimeTests.swift
@@ -11,6 +11,9 @@ import XCTest
 import Foundation
 
 class EngagedTimeTests: XCTestCase {
+    
+    let parsely: Parsely = Parsely.sharedInstance
+    
     override func setUp() {
         super.setUp()
     }
@@ -22,5 +25,22 @@ class EngagedTimeTests: XCTestCase {
     func testSampleFn() {}
     func testHeartbeatFn() {}
     func testStartInteraction() {}
+    func testMultipleTrackedItems() {
+        // should track engagement for different items, separately
+        let itemOne: String = "itemOne"
+        let itemTwo: String = "itemTwo"
+        parsely.startEngagement(id: itemOne)
+        parsely.startEngagement(id: itemTwo)
+        // stop one after 2 seconds
+        // wait 2 seconds
+        parsely.stopEngagement(id: itemOne)
+        // wait another 2 seconds
+        parsely.stopEngagement(id: itemTwo)
+        // they should be tracked separately
+        XCTAssert(parsely.track.engagedTime.accumulators[itemOne]!.id != parsely.track.engagedTime.accumulators[itemTwo]!.id,
+                  "The two items should not be tracked in the same Accumulator")
+        XCTAssert(parsely.track.engagedTime.accumulators[itemOne]!.ms > parsely.track.engagedTime.accumulators[itemTwo]!.ms,
+                  "Waiting for the second item should not add time to the first")
+    }
     func testEndInteraction() {}
 }
