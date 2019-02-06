e326829dfade5fa59756807b7d620f479a51f479
aldraco <a.l.drake713@gmail.com>
2019-02-06 12:53:42 -0700

    Update tests to test for accumulated time (failing).

--- ParselyTrackerTests/EngagedTimeTests.swift
+++ ParselyTrackerTests/EngagedTimeTests.swift
@@ -22,7 +22,17 @@ class EngagedTimeTests: XCTestCase {
         super.tearDown()
     }
     
-    func testSampleFn() {}
+    func testSampleFn() {
+        // accumulators should be able to increase time
+        parsely.startEngagement(id: "sampler-test")
+        sleep(4)
+        parsely.stopEngagement(id: "sampler-test")
+        dump(parsely.track.engagedTime.accumulators["sampler-test"])
+        XCTAssert(parsely.track.engagedTime.accumulators["sampler-test"]!.totalMs > 0,
+                  "The sampler should run as soon as an item is tracked.")
+        XCTAssert(parsely.track.engagedTime.accumulators["sampler-test"]!.totalMs >= 3.8 * 1000,
+                  "The sampler should collect information as long as the item is engaged.")
+    }
     func testHeartbeatFn() {}
     func testStartInteraction() {}
     func testMultipleTrackedItems() {
@@ -32,9 +42,9 @@ class EngagedTimeTests: XCTestCase {
         parsely.startEngagement(id: itemOne)
         parsely.startEngagement(id: itemTwo)
         // stop one after 2 seconds
-        // wait 2 seconds
+        sleep(2)
         parsely.stopEngagement(id: itemOne)
-        // wait another 2 seconds
+        sleep(2)
         parsely.stopEngagement(id: itemTwo)
         // they should be tracked separately
         XCTAssert(parsely.track.engagedTime.accumulators[itemOne]!.id != parsely.track.engagedTime.accumulators[itemTwo]!.id,
