36f8888534adaa772a80379c194516a4f3bc0c0c
aldraco <a.l.drake713@gmail.com>
2019-02-07 13:05:48 -0700

    Remove pixel URL method from Pixel.

--- ParselyTracker/Pixel.swift
+++ ParselyTracker/Pixel.swift
@@ -11,23 +11,6 @@ import SwiftHTTP
 import os.log
 
 class Pixel {
-    var _baseURL: String?
-
-    init() {
-        self._baseURL = nil
-    }
-    
-    func buildPixelURL(now: Date) -> String {
-        // todo: move to http client
-        if self._baseURL == nil {
-            let dateFormatter = DateFormatter()
-            dateFormatter.dateFormat = "yyyy-MM-dd-HH"
-            dateFormatter.timeZone = TimeZone(abbreviation: "UTC")
-            let dateString = dateFormatter.string(from: now)
-            self._baseURL = "https://srv-\(dateString).pixel.parsely.com/mobileproxy/"
-        }
-        return self._baseURL!
-    }
     
     func beacon(additionalParams: Event, shouldNotSetLastRequest: Bool) {
         os_log("Fired beacon", log: OSLog.default, type: .debug)
