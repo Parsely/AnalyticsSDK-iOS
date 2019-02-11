ece018fe82dfde003a7aa8cf58edb8df5d206b0e
aldraco <a.l.drake713@gmail.com>
2019-02-11 15:57:59 -0700

    Move a few things around for clarity.

--- ParselyTracker/Pixel.swift
+++ ParselyTracker/Pixel.swift
@@ -14,9 +14,11 @@ class Pixel {
     
     func beacon(event: Event, shouldNotSetLastRequest: Bool) {
         os_log("Fired beacon", log: OSLog.default, type: .debug)
-        let session = Session().get(extendSession: true)
+        // start forming dictionary
         let rand = Date().millisecondsSince1970
         var data: Dictionary<String,Any?> = ["idsite": Parsely.sharedInstance.apikey, "data": ["ts": rand]]
+        // add session data
+        let session = Session().get(extendSession: true)
         data = data.merging(session, uniquingKeysWith: { (old, _new) in old })
         // add in the event toDict itself
         data = data.merging(event.toDict(), uniquingKeysWith: { (old, _new) in old })
@@ -27,7 +29,7 @@ class Pixel {
         if (shouldNotSetLastRequest) {
             Parsely.sharedInstance.lastRequest = data
         }
-        // TODO: update to enqueue a modified event
+        // TODO: should we just enqueue dicts? this has a duplicate of the event inside data.
         let event = Event(
             data["action"] as! String,
             url: event.url,
