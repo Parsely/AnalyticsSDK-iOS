4217310551b19a55ba985a55edc2b241f0e6a381
aldraco <a.l.drake713@gmail.com>
2019-02-19 16:55:16 -0700

    Implement Metadata class.

--- ParselyTrackerTests/MetadataTests.swift
+++ ParselyTrackerTests/MetadataTests.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+@testable import ParselyTracker
+import XCTest
+
+class MetadataTests: XCTestCase {
+    override func setUp() {
+    }
+    
+    override func tearDown() {
+    }
+    
+    func testMetadataValidation() {
+        // valid metas
+        var metas = ParselyMetadata()
+        // should be able to create nothing, if you want
+        XCTAssert(metas.toDict().isEmpty,
+                  "Empty metadata is just fine")
+        metas = ParselyMetadata(canonical_url: "http://test.com")
+        let expected = ["canonical_url": "http://test.com"]
+        let actual = metas.toDict()
+        XCTAssertEqual(expected as NSObject, actual as NSObject,
+                       "Should handle scant arguments with no issues")
+    }
+}
