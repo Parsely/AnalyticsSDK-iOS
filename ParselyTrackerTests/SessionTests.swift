066669622bfbd2090e3597bdd98811f2330cdaed
aldraco <a.l.drake713@gmail.com>
2019-02-19 13:16:07 -0700

    Update session tests.

--- ParselyTrackerTests/SessionTests.swift
+++ ParselyTrackerTests/SessionTests.swift
@@ -19,8 +19,27 @@ class SessionTests: XCTestCase {
         super.tearDown()
     }
 
-    let parselyTracker: Parsely = Parsely.sharedInstance
+    let sessions = SessionManager()
+    let emptyDict: [String: Any?] = [:]
 
-    func testGet() {}
+    func testGet() {
+        // should be able to create a session if there is none
+        let url1 = "http://parsely-test.com/123"
+        let url2 = "http://parsely-test.com/"
+        let session = sessions.get(url: url1, urlref: "") // will not extend by default
+        XCTAssertFalse(session.isEmpty,
+                  "Should create a session if there is none.")
+        sleep(3)
+        let anotherSession = sessions.get(url: url2, urlref: url1, shouldExtendExisting: true)
+        XCTAssertEqual(session as NSObject, anotherSession as NSObject,
+                          "Should use same session for continued browsing")
+        dump(session)
+        dump(anotherSession)
+        XCTAssert(anotherSession["expires"] as! Date > session["expires"] as! Date,
+                  "Should extend the expiration each time the session is accessed.")
+
+    }
+
+    func testExpiry() {}
 
 }
