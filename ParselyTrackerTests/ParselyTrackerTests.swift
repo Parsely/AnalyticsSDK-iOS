1d879ac5c8f3ee2b297cf10684ed51ded53afb88
Emmett Butler <emmett.butler321@gmail.com>
2019-03-20 22:21:08 +0000

    configure before all tracker-level tests

--- ParselyTrackerTests/ParselyTrackerTests.swift
+++ ParselyTrackerTests/ParselyTrackerTests.swift
@@ -10,22 +10,20 @@ import XCTest
 @testable import ParselyTracker
 
 class ParselyTrackerTests: ParselyTestCase {
+    let testApikey: String = "examplesite.com"
+    
+    override func setUp() {
+        super.setUp()
+        parselyTestTracker.configure(siteId: testApikey)
+    }
+    
     func testConfigure() {
-        let expected = "exampleparsely.com"
-        // XXX this assertion failing indicates a lack of isolation between tests
-        // this should be fixed by having each test set up and tear down its own Parsely object instead of having
-        // all tests use the same Parsely.sharedInstance.
-        XCTAssertEqual(parselyTestTracker.apikey, "",
-                       "Before calls to Parsely.configure, Parsely.apikey should be the empty string")
-        parselyTestTracker.configure(siteId: expected)
-        XCTAssertEqual(parselyTestTracker.apikey, expected,
+        XCTAssertEqual(parselyTestTracker.apikey, testApikey,
                        "After a call to Parsely.configure, Parsely.apikey should be the value used in the call's " +
                        "siteId argument")
     }
     
     func testTrackPageView() {
-        let testApikey = "example.com"
-        parselyTestTracker.configure(siteId: testApikey)
         XCTAssertEqual(parselyTestTracker.eventQueue.length(), 0, "eventQueue should be empty immediately after initialization")
         parselyTestTracker.trackPageView(url: "http://example.com/testurl", urlref: "http://example.com/testurl",
                                          metadata: nil, extraData: nil)
