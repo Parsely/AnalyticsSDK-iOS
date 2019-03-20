8d08ad4ffc7ec70b73ce26676a34e9e8a9d0c107
Emmett Butler <emmett.butler321@gmail.com>
2019-03-20 18:56:36 +0000

    make ParselyTrackerTests a subclass of ParselyTestCase to take advantage of Parsely instance isolation machinery

--- ParselyTrackerTests/ParselyTrackerTests.swift
+++ ParselyTrackerTests/ParselyTrackerTests.swift
@@ -9,27 +9,16 @@
 import XCTest
 @testable import ParselyTracker
 
-class ParselyTrackerTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-    }
-    
-    override func tearDown() {
-        super.tearDown()
-    }
-    
-    let parselyTracker: Parsely = Parsely.sharedInstance
-
+class ParselyTrackerTests: ParselyTestCase {
     func testConfigure() {
         let expected = "exampleparsely.com"
         // XXX this assertion failing indicates a lack of isolation between tests
         // this should be fixed by having each test set up and tear down its own Parsely object instead of having
         // all tests use the same Parsely.sharedInstance.
-        XCTAssertEqual(parselyTracker.apikey, "",
+        XCTAssertEqual(parselyTestTracker.apikey, "",
                        "Before calls to Parsely.configure, Parsely.apikey should be the empty string")
-        parselyTracker.configure(siteId: expected)
-        XCTAssertEqual(parselyTracker.apikey, expected,
+        parselyTestTracker.configure(siteId: expected)
+        XCTAssertEqual(parselyTestTracker.apikey, expected,
                        "After a call to Parsely.configure, Parsely.apikey should be the value used in the call's " +
                        "siteId argument")
     }
