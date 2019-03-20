3c173ed73d54d9b00161eb193b5cd59e288b1500
Emmett Butler <emmett.butler321@gmail.com>
2019-03-20 22:24:51 +0000

    reuse testUrl in tracker-level tests

--- ParselyTrackerTests/ParselyTrackerTests.swift
+++ ParselyTrackerTests/ParselyTrackerTests.swift
@@ -11,6 +11,7 @@ import XCTest
 
 class ParselyTrackerTests: ParselyTestCase {
     let testApikey: String = "examplesite.com"
+    let testUrl = "http://example.com/testurl"
     
     override func setUp() {
         super.setUp()
@@ -24,23 +25,24 @@ class ParselyTrackerTests: ParselyTestCase {
     }
     
     func testTrackPageView() {
-        XCTAssertEqual(parselyTestTracker.eventQueue.length(), 0, "eventQueue should be empty immediately after initialization")
-        parselyTestTracker.trackPageView(url: "http://example.com/testurl", urlref: "http://example.com/testurl",
-                                         metadata: nil, extraData: nil)
+        XCTAssertEqual(parselyTestTracker.eventQueue.length(), 0,
+                       "eventQueue should be empty immediately after initialization")
+        parselyTestTracker.trackPageView(url: testUrl, urlref: testUrl, metadata: nil, extraData: nil)
         XCTAssertEqual(parselyTestTracker.eventQueue.length(), 1,
                        "A call to Parsely.trackPageView should add an event to eventQueue")
     }
     
     func testStartEngagement() {
-        let testUrl = "http://example.com/testurl"
-        parselyTestTracker.startEngagement(url: "http://example.com/testurl")
+        parselyTestTracker.startEngagement(url: testUrl)
         let internalAccumulators:Dictionary<String, Accumulator> = parselyTestTracker.track.engagedTime.accumulators
         let testUrlAccumulator: Accumulator = internalAccumulators[testUrl]!
         XCTAssert(testUrlAccumulator.isEngaged,
                   "After a call to Parsely.startEngagement, the internal accumulator for the engaged url should exist " +
                   "and its isEngaged flag should be set")
     }
-    func testStopEngagement() { XCTAssert(false, "not implemented") }
+    func testStopEngagement() {
+        
+    }
     func testTrackPlay() { XCTAssert(false, "not implemented") }
     func testTrackPause() { XCTAssert(false, "not implemented") }
 }
