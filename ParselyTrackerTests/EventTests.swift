f3e6c7990f0011e71aed2f7608fda91f9e67adf3
Emmett Butler <emmett.butler321@gmail.com>
2019-03-22 23:59:16 +0000

    add failing test for Event.toDict that points out a typo

--- ParselyTrackerTests/EventTests.swift
+++ ParselyTrackerTests/EventTests.swift
@@ -21,7 +21,65 @@ class EventTests: ParselyTestCase {
                   "Should initialize and preserve subclass parameters.")
     }
     
-    func testToDict() { XCTAssert(false, "not implemented") }
+    func testToDict() {
+        let expectedStrings: Dictionary<String, String> = [
+            "action": "pageview",
+            "url": "http://parsely-stuff.com",
+            "urlref": "http://testt.com",
+            "idsite": testApikey,
+            "surl": "http://parsely-stuff.com",
+            "sref": "http://parsely-test.com",
+        ]
+        let expectedInts: Dictionary<String, Int> = [
+            "sid": 0,
+            "sts": 1553295726,
+            "slts": 1553295726
+        ]
+        let extraData: Dictionary<String, String> = [
+            "arbitraryParameter1": "testValue",
+            "arbitraryParameter2": "testValue2"
+        ]
+        let testMetadata: ParselyMetadata = ParselyMetadata(
+            canonical_url: "http://parsely-test.com", pub_date: Date.init(), title: "a title.", authors: ["Yogi Berra"],
+            image_url: "http://parsely-test.com/image2", section: "Things my mother says", tags: ["tag1", "tag2"],
+            duration: TimeInterval(100)
+        )
+        let eventUnderTest = Event(expectedStrings["action"]!, url: expectedStrings["url"]!,
+                                   urlref: expectedStrings["urlref"], metadata: testMetadata,
+                                   extra_data: extraData, idsite: expectedStrings["idsite"]!,
+                                   session_id: expectedInts["sid"], session_timestamp: expectedInts["sts"],
+                                   session_url: expectedStrings["surl"], session_referrer: expectedStrings["sref"],
+                                   last_session_timestamp: expectedInts["slts"])
+        let expectedVisitorID: String = "12345fdffff"
+        eventUnderTest.setVisitorInfo(visitorInfo: ["id": expectedVisitorID])
+        let actual: Dictionary<String, Any> = eventUnderTest.toDict()
+        for (key, value) in expectedStrings {
+            XCTAssertEqual(actual[key]! as! String, value,
+                           "The result of Event.toDict should have the correct " + key + " key")
+        }
+        for (key, value) in expectedInts {
+            XCTAssertEqual(actual[key]! as! Int, value,
+                           "The result of Event.toDict should have the correct " + key + " key")
+        }
+        let actualExtraData: Dictionary<String, Any> = actual["data"] as! Dictionary<String, Any>
+        for (key, value) in extraData {
+            XCTAssertEqual(actualExtraData[key]! as! String, value,
+                           "The result of Event.toDict should have correct values passed via extra_data")
+        }
+        let actualMetadata: Dictionary<String, Any> = actual["metadata"] as! Dictionary<String, Any>
+        let expectedMetadata: Dictionary<String, Any> = testMetadata.toDict()
+        let result: Bool = NSDictionary(dictionary: actualMetadata).isEqual(to: expectedMetadata)
+        XCTAssert(result, "The metadata field of the result of Event.toDict should be a dict representation of the " +
+                          "given metadata")
+        let idKeyExists: Bool = actualExtraData["parsely_site_uuid"] != nil
+        XCTAssert(idKeyExists, "After a call to Event.setVisitorInfo that " +
+                  "passes a visitor dictionary, the parsely_site_uuid key should exist in the extra_data dictionary.")
+        if (idKeyExists) {
+            XCTAssertEqual(actualExtraData["parsely_site_uuid"] as! String, expectedVisitorID,
+                           "A visitor ID provided via Event.setVisitorInfo should be accessible in the result of " +
+                           "Event.toDict as data[\"parsely_site_uuid\"]")
+        }
+    }
     func testEvent() { XCTAssert(false, "not implemented") }
     func testSetVisitorInfo() { XCTAssert(false, "not implemented") }
     func testSetSessionInfo() { XCTAssert(false, "not implemented") }
