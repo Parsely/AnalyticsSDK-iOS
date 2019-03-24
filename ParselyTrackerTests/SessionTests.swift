4adf1bb3b74bc191d0ca1f6c7a959a3130cbf792
Emmett Butler <emmett.butler321@gmail.com>
2019-03-24 20:31:22 +0000

    add test for session fields

--- ParselyTrackerTests/SessionTests.swift
+++ ParselyTrackerTests/SessionTests.swift
@@ -3,24 +3,43 @@ import XCTest
 
 class SessionTests: ParselyTestCase {
     var sessions: SessionManager!
-    let emptyDict: [String: Any?] = [:]
+    let testInitialUrl = "http://parsely-test.com/123"
+    let testSubsequentUrl = "http://parsely-test.com/"
+    let epochTimeInThePast = 1553459222
     
     override func setUp() {
         super.setUp()
         sessions = SessionManager(trackerInstance: parselyTestTracker)
     }
-
+    
     func testGet() {
-        let url1 = "http://parsely-test.com/123"
-        let url2 = "http://parsely-test.com/"
-        let session = sessions.get(url: url1, urlref: "")
+        let session = sessions.get(url: testInitialUrl, urlref: testSubsequentUrl)
+        XCTAssertGreaterThanOrEqual(session["session_id"] as! Int, 0,
+                                    "The session_id of a newly-created session should be greater than or equal to 0")
+        XCTAssertEqual(session["session_url"] as! String, testInitialUrl,
+                       "The session_url of a newly-created session should be the url it was initialized with")
+        XCTAssertEqual(session["session_referrer"] as! String, testSubsequentUrl,
+                       "The session_referrer of a newly-created session should be the urlref it was initialized with")
+        XCTAssertGreaterThan(session["session_ts"] as! Int, epochTimeInThePast,
+                             "The session_ts of a newly-created session should be non-ancient")
+        XCTAssertGreaterThan(session["last_session_ts"] as! Int, epochTimeInThePast,
+                             "The last_session_ts of a newly-created session should be non-ancient")
+    }
+
+    func testIDPersists() {
+        let session = sessions.get(url: testInitialUrl, urlref: "")
         XCTAssertFalse(session.isEmpty, "The first call to SessionManager.get should create a session object")
-        let subsequentSession = sessions.get(url: url2, urlref: url1, shouldExtendExisting: true)
+        let subsequentSession = sessions.get(url: testSubsequentUrl, urlref: testInitialUrl, shouldExtendExisting: true)
         XCTAssertEqual(session["session_id"] as! Int, subsequentSession["session_id"] as! Int,
                        "Sequential calls to SessionManager.get within the session timeout that have " +
                        "shouldExtendExisting:true should return a session object with the same session ID as the " +
                        "preexisting session object")
-        XCTAssert(false, "Other properites of the session should match their expected values")
+        XCTAssertEqual(session["session_url"] as! String, testInitialUrl,
+                       "The url of a session that has been extended with a different url should not have changed")
+    }
+    
+    func testGetCorrectlyMutatesVisitor() {
+        XCTAssert(false, "not implemented")
     }
 
     func testShouldExtendExisting() {
