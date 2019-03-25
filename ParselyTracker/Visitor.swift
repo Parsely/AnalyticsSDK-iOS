0549da3cd3656d752bb931e4b77b08c49ec112ad
Emmett Butler <emmett.butler321@gmail.com>
2019-03-25 22:57:35 +0000

    lock up the members of visitormanager

--- ParselyTracker/Visitor.swift
+++ ParselyTracker/Visitor.swift
@@ -3,9 +3,9 @@ import Foundation
 class VisitorManager {
     private let VISITOR_TIMEOUT: TimeInterval = 60 * 60 * 24 * 365  / 12 * 13 // 13 months
     private let storage = Parsely.sharedStorage
-    let visitorKey = "_parsely_visitor_uuid"
+    private let visitorKey = "_parsely_visitor_uuid"
 
-    func getVisitorInfo(shouldExtendExisting: Bool = false) -> Dictionary<String, Any?> {
+    internal func getVisitorInfo(shouldExtendExisting: Bool = false) -> Dictionary<String, Any?> {
         var visitorInfo = self.storage.get(key: self.visitorKey) ?? [:]
         if (visitorInfo.isEmpty) {
             visitorInfo = self.initVisitor(visitorId: UUID().uuidString)
@@ -15,7 +15,7 @@ class VisitorManager {
         return visitorInfo
     }
 
-    func initVisitor(visitorId: String) -> Dictionary<String, Any?> {
+    private func initVisitor(visitorId: String) -> Dictionary<String, Any?> {
        return self.setVisitorInfo(visitorInfo: [
         "id": visitorId,
         "session_count": 0,
@@ -23,11 +23,11 @@ class VisitorManager {
        ])
     }
     
-    func setVisitorInfo(visitorInfo: Dictionary<String, Any?>) -> Dictionary<String, Any?> {
+    internal func setVisitorInfo(visitorInfo: Dictionary<String, Any?>) -> Dictionary<String, Any?> {
         return storage.set(key: visitorKey, value: visitorInfo, expires: Date.init(timeIntervalSinceNow: self.VISITOR_TIMEOUT))
     }
     
-    func extendVisitorExpiry() -> Dictionary<String, Any?> {
+    private func extendVisitorExpiry() -> Dictionary<String, Any?> {
         return storage.extendExpiry(key: visitorKey, expires: Date.init(timeIntervalSinceNow: self.VISITOR_TIMEOUT)) ?? [:]
     }
 }
