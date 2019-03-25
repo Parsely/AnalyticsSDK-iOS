ae2114f87dec726a66c106260185051d926fb583
Emmett Butler <emmett.butler321@gmail.com>
2019-03-25 00:33:58 +0000

    use a test-specific instance of EngagedTime for tests

--- ParselyTrackerTests/EngagedTimeTests.swift
+++ ParselyTrackerTests/EngagedTimeTests.swift
@@ -2,24 +2,30 @@ import XCTest
 @testable import ParselyTracker
 
 class EngagedTimeTests: ParselyTestCase {
+    var engagedTime: EngagedTime?
     let testUrl: String = "http://parsely-stuff.com"
     
+    override func setUp() {
+        super.setUp()
+        engagedTime = EngagedTime(trackerInstance: parselyTestTracker)
+    }
+    
     func testHeartbeatFn() {
-        let dummyEventArgs: Dictionary<String, Any> = parselyTestTracker.track.engagedTime.generateEventArgs(
+        let dummyEventArgs: Dictionary<String, Any> = engagedTime!.generateEventArgs(
             url: testUrl, urlref: "", extra_data: nil, idsite: ParselyTestCase.testApikey)
         let dummyAccumulator: Accumulator = Accumulator(key: "", accumulatedTime: 0, totalTime: 0,
                                                         lastSampleTime: Date(), lastPositiveSampleTime: Date(),
                                                         heartbeatTimeout: 0, contentDuration: 0, isEngaged: false,
                                                         eventArgs: dummyEventArgs)
-        parselyTestTracker.track.engagedTime.heartbeatFn(data: dummyAccumulator, enableHeartbeats: true)
+        engagedTime!.heartbeatFn(data: dummyAccumulator, enableHeartbeats: true)
         XCTAssertEqual(parselyTestTracker.eventQueue.length(), 1,
                        "A call to Parsely.track.engagedTime.heartbeatFn should add an event to eventQueue")
     }
     
     func testStartInteraction() {
-        parselyTestTracker.track.engagedTime.startInteraction(url: testUrl, urlref: "", extra_data: nil,
-                                                              idsite: ParselyTestCase.testApikey)
-        let internalAccumulators:Dictionary<String, Accumulator> = parselyTestTracker.track.engagedTime.accumulators
+        engagedTime!.startInteraction(url: testUrl, urlref: "", extra_data: nil,
+                                      idsite: ParselyTestCase.testApikey)
+        let internalAccumulators:Dictionary<String, Accumulator> = engagedTime!.accumulators
         let testUrlAccumulator: Accumulator = internalAccumulators[testUrl]!
         XCTAssert(testUrlAccumulator.isEngaged,
                   "After a call to Parsely.track.engagedTime.startInteraction, the internal accumulator for the engaged " +
@@ -27,10 +33,10 @@ class EngagedTimeTests: ParselyTestCase {
     }
     
     func testEndInteraction() {
-        parselyTestTracker.track.engagedTime.startInteraction(url: testUrl, urlref: "", extra_data: nil,
-                                                              idsite: ParselyTestCase.testApikey)
-        parselyTestTracker.track.engagedTime.endInteraction()
-        let internalAccumulators:Dictionary<String, Accumulator> = parselyTestTracker.track.engagedTime.accumulators
+        engagedTime!.startInteraction(url: testUrl, urlref: "", extra_data: nil,
+                                      idsite: ParselyTestCase.testApikey)
+        engagedTime!.endInteraction()
+        let internalAccumulators:Dictionary<String, Accumulator> = engagedTime!.accumulators
         let testUrlAccumulator: Accumulator = internalAccumulators[testUrl]!
         XCTAssertFalse(testUrlAccumulator.isEngaged,
                        "After a call to Parsely.track.engagedTime.startInteraction followed by a call to " +
