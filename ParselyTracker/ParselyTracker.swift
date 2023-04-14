eb2474f47dea16c72b7213e57e71f1e48d99207f
Tony Li <tony.li@automattic.com>
2023-04-14 14:48:39 +1200

    Do not use an global queue
    The function is now executed in the event processor queue.

--- ParselyTracker/ParselyTracker.swift
+++ ParselyTracker/ParselyTracker.swift
@@ -277,17 +277,14 @@ public class Parsely {
         self.active = false
         os_log("Stopping execution before background/inactive/terminate", log:OSLog.tracker, type:.info)
         hardShutdown()
-        
-        DispatchQueue.global(qos: .userInitiated).async{
-            let _self = self
-            _self.backgroundFlushTask = UIApplication.shared.beginBackgroundTask(expirationHandler:{
-                _self.endBackgroundFlushTask()
-            })
-            os_log("Flushing queue in background", log:OSLog.tracker, type:.info)
-            _self.track.sendHeartbeats()
-            _self.flush()
-            _self.endBackgroundFlushTask()
-        }
+
+        self.backgroundFlushTask = UIApplication.shared.beginBackgroundTask(expirationHandler:{
+            self.endBackgroundFlushTask()
+        })
+        os_log("Flushing queue in background", log:OSLog.tracker, type:.info)
+        self.track.sendHeartbeats()
+        self.flush()
+        self.endBackgroundFlushTask()
     }
     
     internal func hardShutdown() {
