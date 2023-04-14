793429a3ac9d14c7e2435238d8b12c90e88727c6
Gio Lodi <gio@mokacoding.com>
2023-04-14 15:51:21 +1000

    Reintroduce `window` property in demo app `AppDelegate`
    This was removed in 1be7502, but it was a mistake as it's required at
    runtime by iOS.

--- ParselyDemo/AppDelegate.swift
+++ ParselyDemo/AppDelegate.swift
@@ -4,6 +4,12 @@ import ParselyTracker
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
+    // This is unused but necessary in a Storyboard without UISceneDelegate setup.
+    // Without it, the app will show a black screen and log:
+    //
+    // > The app delegate must implement the window property if it wants to use a main storyboard file.
+    var window: UIWindow?
+
     var parsely: Parsely!
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
