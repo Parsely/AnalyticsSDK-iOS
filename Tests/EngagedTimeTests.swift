9b589b4ad42f5b26110ce7dfbefda30eef4d759c
Gio Lodi <gio@mokacoding.com>
2023-04-19 16:14:18 +1000

    Add call to `configure(siteId:)` to test fail in new setup
    I think the reason the tests started to fail with this setup is that the
    path to the host app for the unit tests changed and that, for some
    reason still unknown to me, seem to clash with the configure call
    execution.

--- Tests/EngagedTimeTests.swift
+++ Tests/EngagedTimeTests.swift
@@ -68,6 +68,9 @@ class EngagedTimeTests: ParselyTestCase {
     
    
     func testGlobalPause() {
+        // This is call to configure required for the start-stop mechanism to work
+        sharedInstance?.configure(siteId: ParselyTestCase.testApikey)
+
         let assertionTimeout:TimeInterval = TimeInterval(3)
         let acceptableDifference:TimeInterval = TimeInterval(0.2)
 
